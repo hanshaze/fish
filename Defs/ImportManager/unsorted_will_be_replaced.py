f0f6caf8f10ef9ee6b29d2273e3cd8ecf059f43b
sTiKyt <stikyt@protonmail.com>
2020-05-06 14:26:31 +0300

    created unsorted_will_be_replaced.py to handle all imports

--- Defs/ImportManager/unsorted_will_be_replaced.py
+++ Defs/ImportManager/unsorted_will_be_replaced.py
@@ -1,2 +1,5 @@
-from subprocess import call as run_command
+from subprocess import call as run_command, check_output, CalledProcessError
 from distutils.dir_util import copy_tree as webpage_set
+from time import sleep as wait
+from os import path, system
+import re as regular_expression
