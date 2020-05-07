896be7e335ed9350561b7596a6b6f0369e60d23e
sTiKyt <stikyt@protonmail.com>
2020-05-07 21:01:47 +0300

    Imported Popen, DEVNULL, mkdir, remove, chdir

--- Defs/ImportManager/unsorted_will_be_replaced.py
+++ Defs/ImportManager/unsorted_will_be_replaced.py
@@ -1,7 +1,7 @@
-from subprocess import call as run_command, check_output, CalledProcessError
+from subprocess import call as run_command,Popen as run_background_command, check_output, CalledProcessError, DEVNULL
 from distutils.dir_util import copy_tree as webpage_set
 from time import sleep as wait
-from os import path, system, chmod, stat
+from os import path, system, chmod, stat, mkdir, remove, chdir
 from shutil import rmtree, copyfile
 from pathlib import Path as pathlib_Path
 import re as regular_expression
