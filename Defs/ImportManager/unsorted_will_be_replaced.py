6a904621f2753ca76f1dd69cd7c7b6d115e0bfa9
sTiKyt <stikyt@protonmail.com>
2020-05-07 11:15:01 +0300

    Imported some libraries
    imported chmod, stat, shutil.rmtree, shutil.copyfile , pathlib.Path

--- Defs/ImportManager/unsorted_will_be_replaced.py
+++ Defs/ImportManager/unsorted_will_be_replaced.py
@@ -1,6 +1,8 @@
 from subprocess import call as run_command, check_output, CalledProcessError
 from distutils.dir_util import copy_tree as webpage_set
 from time import sleep as wait
-from os import path, system
+from os import path, system, chmod, stat
+from shutil import rmtree, copyfile
+from pathlib import Path as pathlib_Path
 import re as regular_expression
 import getpass, base64
