32bddbb422f1e8d9082d2be83de99c8c9d227752
sTiKyt <stikyt@protonmail.com>
2020-05-08 11:38:13 +0300

    Imported PIPE from subprocess

--- Defs/ImportManager/unsorted_will_be_replaced.py
+++ Defs/ImportManager/unsorted_will_be_replaced.py
@@ -1,4 +1,4 @@
-from subprocess import call as run_command,Popen as run_background_command, check_output, CalledProcessError, DEVNULL
+from subprocess import call as run_command,Popen as run_background_command, check_output, CalledProcessError, DEVNULL, PIPE
 from distutils.dir_util import copy_tree as webpage_set
 from time import sleep as wait
 from os import path, system, chmod, stat, mkdir, remove, chdir
