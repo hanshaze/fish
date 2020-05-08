a12165d8e7e8c91c220610adcb70d2da93a9dc5f
sTiKyt <stikyt@protonmail.com>
2020-05-08 18:20:06 +0300

    Imported replace

--- Defs/ImportManager/unsorted_will_be_replaced.py
+++ Defs/ImportManager/unsorted_will_be_replaced.py
@@ -1,7 +1,7 @@
 from subprocess import call as run_command,Popen as run_background_command, check_output, CalledProcessError, DEVNULL, PIPE
 from distutils.dir_util import copy_tree as webpage_set
 from time import sleep as wait
-from os import path, system, chmod, stat, mkdir, remove, chdir
+from os import path, system, chmod, stat, mkdir, remove, chdir, replace
 from shutil import rmtree, copyfile
 from pathlib import Path as pathlib_Path
 from pyngrok import ngrok
