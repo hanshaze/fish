b42a431b9525898e2330944f10998cda77dfda64
sTiKyt <stikyt@protonmail.com>
2020-05-10 15:57:47 +0300

    Created global string input_line

--- Defs/LocalizationManager/lang_global_usage.py
+++ Defs/LocalizationManager/lang_global_usage.py
@@ -7,3 +7,5 @@ hidden_eye_logo = """
  {1} ███████ ██ ██    ██ ██    ██ ███████ ██ ██ ██  {2}███████   ████   ███████ {0}
  {1} ██   ██ ██ ██    ██ ██    ██ ██      ██  ████  {2}██         ██    ██      {0}
  {1} ██   ██ ██ ██████   ██████   ███████ ██   ███  {2}███████    ██    ███████ {0}""".format(default_palette[4], default_palette[2], default_palette[0])
+
+input_line = "\n{0}HiddenEye >>>  {1}".format(default_palette[0], default_palette[2])
