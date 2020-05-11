e4d0be366ebbf0cc23a0f4cdf562f60a1d39ad39
sTiKyt <stikyt@protonmail.com>
2020-05-11 15:17:42 +0300

    Integrated server_menu into localization system

--- Defs/ActionManager/Server/server_menu.py
+++ Defs/ActionManager/Server/server_menu.py
@@ -9,6 +9,9 @@
 from Defs.ImportManager.unsorted_will_be_replaced import run_command
 import Defs.ThemeManager.theme as theme
 import Defs.ActionManager.Server.server_runner as server_runner
+import Defs.LocalizationManager.lang_action_manager.lang_server.lang_server_menu as localization
+import Defs.LocalizationManager.lang_global_usage as global_localization
+import Defs.ActionManager.main_runner as main_runner
 
 default_palette = theme.default_palette
 
@@ -20,36 +23,41 @@ default_palette = theme.default_palette
 
 def server_selection(port):  # Question where user must select server
     run_command('clear')
-    print('''
-        {1}_  _ . ___  ___  ___ _  _  {0}___ _  _ ___{1}
-        |__| | ]  | ]  | |__ |\ |  {0}|__ \__/ |__{1}
-        |  | | ]__| ]__| |__ | \|  {0}|__  ||  |__{1}
-        {0}http://github.com/darksecdevelopers
-        {0}** BY:DARKSEC ** \n\n-------------------------------\n{0}[ HOST SERVER SELECTION ]{1}!! {0}\n-------------------------------'''.format(default_palette[0], default_palette[2]))
-    print(
-        "\n {0}[{1}*{0}]{0}Select Any Available Server:{1}".format(default_palette[0], default_palette[4]))
-    print("\n {0}[{1}0{0}]{1}LOCALHOST \n {0}[{1}1{0}]{1}Ngrok\n {0}[{1}2{0}]{1}Serveo {0}(Currently DOWN)\n {0}[{1}3{0}]{1}Localxpose\n {0}[{1}4{0}]{1}Localtunnel \n {0}[{1}5{0}]{1}OpenPort\n {0}[{1}6{0}]{1}Pagekite\n".format(default_palette[0], default_palette[2]))
+    #print('''
+    #    {1}_  _ . ___  ___  ___ _  _  {0}___ _  _ ___{1}
+    #    |__| | ]  | ]  | |__ |\ |  {0}|__ \__/ |__{1}
+    #    |  | | ]__| ]__| |__ | \|  {0}|__  ||  |__{1}
+    #    {0}http://github.com/darksecdevelopers
+    #    {0}** BY:DARKSEC ** \n\n-------------------------------\n
 
-    choice = input(" \n{0}HiddenEye >>> {1}".format(default_palette[0], default_palette[2]))
-    if choice == '0':
+    # )
+    print(global_localization.hidden_eye_logo)
+    print(global_localization.official_website_link)
+    print(global_localization.by_darksec)
+    print(localization.lang_server_selection["server_selection"])
+    print(localization.lang_server_selection["select_any_available_server"])
+    main_runner.print_sorted_as_menu(localization.lang_server_selection["servers_list"])
+    choice = input(global_localization.input_line)
+    choice = choice.zfill(2)
+    if choice == '00':
         run_command('clear')
         server_runner.start_localhost(port) #FIXED
-    elif choice == '1':
+    elif choice == '01':
         run_command('clear')
         server_runner.start_ngrok(port) # FIXED
-    elif choice == '2':
+    elif choice == '02':
         run_command('clear')
         server_runner.start_serveo(port) # ALMOST FIXED
-    elif choice == '3':
+    elif choice == '03':
         run_command('clear')
         server_runner.start_localxpose(port) # DOESN'T GET ENTERED CREDENTIALS BACK
-    elif choice == '4':
+    elif choice == '04':
         run_command('clear')
         server_runner.start_localtunnel(port, True)
-    elif choice == '5':
+    elif choice == '05':
         run_command('clear')
         server_runner.start_openport(port)
-    elif choice == '6':
+    elif choice == '06':
         run_command('clear')
         server_runner.start_pagekite(port)
     else:
