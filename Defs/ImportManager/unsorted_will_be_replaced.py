a273eb3a7a57fdac0e2f4a1add36bef8fb373909
sTiKyt <stikyt@protonmail.com>
2020-05-13 18:44:27 +0300

    Imported check_call, getuid

--- Defs/ImportManager/unsorted_will_be_replaced.py
+++ Defs/ImportManager/unsorted_will_be_replaced.py
@@ -6,12 +6,14 @@
 #
 
 
-from subprocess import call as run_command,Popen as run_background_command, check_output, CalledProcessError, DEVNULL, PIPE
+from subprocess import call as run_command, check_call as try_to_run_command ,Popen as run_background_command, check_output, CalledProcessError, DEVNULL, PIPE
 from distutils.dir_util import copy_tree as webpage_set
 from time import sleep as wait
-from os import path, system, chmod, stat, mkdir, remove, chdir, replace
+from os import path, system, chmod, stat, mkdir, remove, chdir, replace, getuid
 from shutil import rmtree, copyfile
 from pathlib import Path as pathlib_Path
 from pyngrok import ngrok
 import re as regular_expression
 import getpass, base64, socket, requests
+from platform import system as platform_os
+import requests
