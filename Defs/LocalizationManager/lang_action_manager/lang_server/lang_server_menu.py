6ebd852a2bac6e6e3df2407950d6d5f92cd1afb3
sTiKyt <stikyt@protonmail.com>
2020-05-13 18:45:04 +0300

    Added message about not working options

--- Defs/LocalizationManager/lang_action_manager/lang_server/lang_server_menu.py
+++ Defs/LocalizationManager/lang_action_manager/lang_server/lang_server_menu.py
@@ -14,8 +14,8 @@ lang_server_selection = {
     "server_selection" : _('{0}[ HOSTING SERVER SELECTION ]{1}! {0}\n-------------------------------').format(default_palette[0], default_palette[2]),
     "select_any_available_server" : _('\n {0}[{1}*{0}]{0}Select Any Available Server:{1}').format(default_palette[0], default_palette[4]),
     "servers_list" :
-    [ ['{0}[{1}00{0}]{1}Localhost', '{0}[{1}04{0}]{1}Localtunnel'],
-      ['{0}[{1}01{0}]{1}Ngrok', '{0}[{1}05{0}]{1}OpenPort'],
-      ['{0}[{1}02{0}]{1}Serveo', '{0}[{1}06{0}]{1}Pagekite'],
-      ['{0}[{1}03{0}]{1}Localxpose']]
+    [ ['{0}[{1}00{0}]{1}Localhost', '{0}[{1}04{0}]{1}Localtunnel (not working now)'],
+      ['{0}[{1}01{0}]{1}Ngrok', '{0}[{1}05{0}]{1}OpenPort (not working now)'],
+      ['{0}[{1}02{0}]{1}Serveo', '{0}[{1}06{0}]{1}Pagekite (not working now)'],
+      ['{0}[{1}03{0}]{1}Localxpose (not working now)']]
 }
