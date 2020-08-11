6b86115a130a2b3473a66bcb220949d174be8d95
sTiKyt <stikyt@protonmail.com>
2020-08-11 21:02:26 +0300

    Fix

--- views/EULA_view.py
+++ views/EULA_view.py
@@ -6,7 +6,7 @@ class EULAView:
         self.EULA_messages = {
             "eula_found": _("EULA is found"),
             "eula_is_confirmed": _("You accepted EULA"),
-            "eula_is_not_confirmed": _("You didn't accept EULA"),
+            "eula_is_not_confirmed": _("You didn't accept EULA, please open eula.txt"),
             "eula_not_found": _("EULA isn't found"),
             "eula_is_invalid": _("EULA is not valid"),
             "eula_start_of_file": _("# Please read and accept EULA below\n eula = False")
