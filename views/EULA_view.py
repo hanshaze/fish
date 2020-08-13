6ee10a90aaedaea0c510fd3469d4080d16e29606
sTiKyt <stikyt@protonmail.com>
2020-08-13 13:27:44 +0300

    Added more debug info to "eula_not_found"

--- views/EULA_view.py
+++ views/EULA_view.py
@@ -7,7 +7,7 @@ class EULAView:
             "eula_found": _("EULA is found"),
             "eula_is_confirmed": _("You accepted EULA"),
             "eula_is_not_confirmed": _("You didn't accept EULA, please open eula.txt"),
-            "eula_not_found": _("EULA isn't found"),
+            "eula_not_found": _("EULA isn't found\n Generated new EULA"),
             "eula_is_invalid": _("EULA is not valid"),
             "eula_start_of_file_unconfirmed": _("# Please read and accept EULA below\n eula = False"),
         }
