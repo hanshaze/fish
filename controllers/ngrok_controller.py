7dece996b050b6dc1f739e07febe0174530f71a7
sTiKyt <stikyt@protonmail.com>
2020-08-22 18:42:52 +0000

    Created obtain_tunnels method for ngrok

--- controllers/ngrok_controller.py
+++ controllers/ngrok_controller.py
@@ -30,3 +30,6 @@ class NgrokController:
 
     def establish_connection(self, port='80'):
         self._model.connect(port=port, name='HiddenEye Connection', pyngrok_config=self._config_path)
+
+    def obtain_tunnels(self):
+        self._tunnels = self._model.get_tunnels()
